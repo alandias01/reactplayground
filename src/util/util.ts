3eee7e5ea6c465248db4a10944733636543d943f
alan <alandias01@yahoo.com>
2021-02-12 10:35:31 -0500

    added util folder

---
+++ src/util/util.ts
@@ -0,0 +1,14 @@
+/**
+ * A promise that resolves or rejects after a delay time and data to return if resolved after that delay
+ * @param timeToDelay
+ * @param data
+ * @param shouldResolve
+ */
+export const TaskDelay = <T extends {}>(
+  timeToDelay: number,
+  data: T = {} as T,
+  shouldResolve = true
+): Promise<T> =>
+  new Promise((res, rej) =>
+    setTimeout(() => (shouldResolve ? res(data) : rej()), timeToDelay)
+  );
